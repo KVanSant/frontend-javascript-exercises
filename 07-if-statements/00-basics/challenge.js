c74a96499f013161f1a201f50c2d3f1b55f17a92
Kristopher Van Sant <kristopher@signaltransmit.com>
2014-12-09 21:11:03 +0000

    if/else statements completed

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,49 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess) {
+  if (guess > fav) {
+    return "Too high";
+  } 
+  else if (guess === fav) {
+    return "You got it!";
+  }
+  else {
+    return "Too low";
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(first, second, third, fourth) {
+  if (first != 3 && first != 5 && first !=7) {
+    return "incorrect";
+  }
+  else if (second != 2) {
+    return "incorrect";
+  }
+  else if (third < 5 || third > 100) {
+    return "incorrect";
+  }
+  else if (9 <= fourth && fourth <= 20) {
+    return "incorrect";
+  }
+  else {
+    return "correct";
+  }
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  if (item == "MacBook Air" && money >=999){
+    return true;
+  }
+  else if (item == "MacBook Pro" && money >= 1299){
+    return true;
+  }
+  else if (item == "Mac Pro" && money >= 2499){
+    return true;
+  }  
+  else if (item == "Apple Sticker" && money >= 1){
+    return true;
+  }
+  else {
+    return false;
+  }
+  
+  };
+
